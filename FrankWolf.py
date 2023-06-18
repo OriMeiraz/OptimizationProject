89812cfe52e621ef3171666b45a4d05cf34c6e09
OriMeiraz <ori.meiraz@campus.technion.ac.il>
2023-06-18 19:58:21 +0300

    created the frank wolfe algorithm

--- FrankWolf.py
+++ FrankWolf.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import linalg as LA
 from utils import calc_L, matrix_dot, get_LB_UB, get_delta
+from math import sqrt
 
 
 def bisection(cov, D, radius, tol):
@@ -28,3 +29,26 @@ def bisection(cov, D, radius, tol):
             break
 
     return L
+
+
+def frank_wolfe(cov, radius, tol, n: int, max_iter=1000):
+    sigma_low = np.min(LA.eigvals(cov))
+    sigma_high = (radius + sqrt(cov.trace()))**2
+    C_high = 2 * np.power(sigma_high, 4) / np.power(sigma_low, 3)
+    S = cov
+    k = 0
+    stoping_criterion = False
+    while k < max_iter and not stoping_criterion:
+        alpha = 2/(k+2)
+        S_xy = S[:n, n:]
+        S_yy = S[n:, n:]
+        G = S_xy @ LA.inv(S_yy)
+        In_G = np.concatenate((np.eye(n), G), axis=1)
+        D = In_G.T @ In_G
+        eps = alpha * tol * C_high
+        L = bisection(cov, D, radius, eps)
+        S += alpha * (L - S)
+        k += 1
+        stoping_criterion = True
+        raise NotImplementedError()
+    return S
